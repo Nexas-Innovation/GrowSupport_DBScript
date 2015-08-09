19c7fcb4976cca552767b20b69c0883ec8d43f19
dev-kanage <developer.kanage0127@gmail.com>
2015-08-09 14:16:03 +0900

    PK追加

--- T_JinendoTasseiMokuhyoSettei.sql
+++ T_JinendoTasseiMokuhyoSettei.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[T_JinendoTasseiMokuhyoSettei] (
     [HyokaNendo]         CHAR (4)      NOT NULL,
     [TasseiMokuhyoSbtCd] CHAR (3)      NULL,
     [TasseiMokuhyoNaiyo] NVARCHAR (50) NULL,
-    [DelFlg]             CHAR (1)      NOT NULL DEFAULT 0,
+    [DelFlg]             CHAR (1)      DEFAULT ((0)) NOT NULL,
     [CrtShainCd]         CHAR (3)      NOT NULL,
     [CrtDt]              DATETIME2 (7) NOT NULL,
     [CrtPlgId]           VARCHAR (10)  NOT NULL,
@@ -12,6 +12,7 @@ CREATE TABLE [dbo].[T_JinendoTasseiMokuhyoSettei] (
     [LastUpdDt]          DATETIME2 (7) NOT NULL,
     [LastUpdPlgId]       VARCHAR (10)  NOT NULL,
     [RowVersion]         ROWVERSION    NOT NULL,
+    CONSTRAINT [PK_T_JinendoTasseiMokuhyoSettei] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_T_JinendoTasseiMokuhyoSettei_T_MokuhyoTasseidoJikoHyoka_1] FOREIGN KEY ([ShainCd], [HyokaNendo]) REFERENCES [dbo].[T_MokuhyoTasseidoJikoHyoka] ([ShainCd], [HyokaNendo]),
     CONSTRAINT [FK_T_JinendoTasseiMokuhyoSettei_M_TasseiMokuhyoSbt_2] FOREIGN KEY ([HyokaNendo], [TasseiMokuhyoSbtCd]) REFERENCES [dbo].[M_TasseiMokuhyoSbt] ([HyokaNendo], [TasseiMokuhyoSbtCd])
 );
